481e41321f23d64319610174e91aa0b0495ddb05
Denis Buriakov <buryakovdv@gmail.com>
2022-07-06 22:24:04 +0400

    add searchcontroller

--- lib/widgets/movie_list/movie_list_widget.dart
+++ lib/widgets/movie_list/movie_list_widget.dart
@@ -13,7 +13,14 @@ class Movie {
       required this.description});
 }
 
-class MovieListWidget extends StatelessWidget {
+class MovieListWidget extends StatefulWidget {
+  MovieListWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MovieListWidget> createState() => _MovieListWidgetState();
+}
+
+class _MovieListWidgetState extends State<MovieListWidget> {
   final _movies = [
     Movie(
         imageName: 'images/movie_placeholder.png',
@@ -23,19 +30,19 @@ class MovieListWidget extends StatelessWidget {
             'Set in ninth-century China during a time of unrest that would eventually lead to the decline of the Tand Dynasty, "The Assassin" tells the story of Nie Yinniang (Shu Qui), who was kidnapped by her family when she was only 10 by Jiaxin (Sheu Fang-yi), a nun who trained her to become a brutally efficient assassin'),
     Movie(
         imageName: 'images/movie_placeholder.png',
-        title: 'Смертельная битва',
+        title: 'Дашка ',
         time: 'April 7, 2021',
         description:
             'Set in ninth-century China during a time of unrest that would eventually lead to the decline of the Tand Dynasty, "The Assassin" tells the story of Nie Yinniang (Shu Qui), who was kidnapped by her family when she was only 10 by Jiaxin (Sheu Fang-yi), a nun who trained her to become a brutally efficient assassin'),
     Movie(
         imageName: 'images/movie_placeholder.png',
-        title: 'Смертельная битва',
+        title: 'Какашка',
         time: 'April 7, 2021',
         description:
             'Set in ninth-century China during a time of unrest that would eventually lead to the decline of the Tand Dynasty, "The Assassin" tells the story of Nie Yinniang (Shu Qui), who was kidnapped by her family when she was only 10 by Jiaxin (Sheu Fang-yi), a nun who trained her to become a brutally efficient assassin'),
     Movie(
         imageName: 'images/movie_placeholder.png',
-        title: 'Смертельная битва',
+        title: 'Антилопа гну',
         time: 'April 7, 2021',
         description:
             'Set in ninth-century China during a time of unrest that would eventually lead to the decline of the Tand Dynasty, "The Assassin" tells the story of Nie Yinniang (Shu Qui), who was kidnapped by her family when she was only 10 by Jiaxin (Sheu Fang-yi), a nun who trained her to become a brutally efficient assassin'),
@@ -47,77 +54,118 @@ class MovieListWidget extends StatelessWidget {
             'Set in ninth-century China during a time of unrest that would eventually lead to the decline of the Tand Dynasty, "The Assassin" tells the story of Nie Yinniang (Shu Qui), who was kidnapped by her family when she was only 10 by Jiaxin (Sheu Fang-yi), a nun who trained her to become a brutally efficient assassin'),
   ];
 
-  MovieListWidget({Key? key}) : super(key: key);
+  var _filteredMovies = <Movie>[];
+
+  final _searchController = TextEditingController();
+
+  void _searchMovies() {
+    final query = _searchController.text;
+    if (query.isNotEmpty) {
+      _filteredMovies = _movies.where((Movie movie) {
+        return movie.title.toLowerCase().contains(query.toLowerCase());
+      }).toList();
+    } else {
+      _filteredMovies = _movies;
+    }
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _searchController.addListener(_searchMovies);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: _movies.length,
-        itemExtent: 150,
-        itemBuilder: (BuildContext context, int index) {
-          final movie = _movies[index];
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
-            child: Stack(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(color: Colors.black.withOpacity(0.2)),
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.1),
-                          blurRadius: 8,
-                          offset: Offset(0, 2),
-                        )
-                      ]),
-                  clipBehavior: Clip.hardEdge,
-                  child: Row(
-                    children: [
-                      Image(image: AssetImage(movie.imageName)),
-                      SizedBox(width: 15),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(height: 15),
-                            Text(
-                              movie.title,
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            SizedBox(height: 5),
-                            Text(movie.time,
-                                maxLines: 1,
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(color: Colors.grey)),
-                            SizedBox(height: 15),
-                            Text(
-                              movie.description,
-                              maxLines: 2,
-                              overflow: TextOverflow.ellipsis,
+    return Stack(
+      children: [
+        ListView.builder(
+            padding: EdgeInsets.only(top: 70.0),
+            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+            itemCount: _filteredMovies.length,
+            itemExtent: 150,
+            itemBuilder: (BuildContext context, int index) {
+              final movie = _filteredMovies[index];
+              return Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
+                child: Stack(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          border:
+                              Border.all(color: Colors.black.withOpacity(0.2)),
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.1),
+                              blurRadius: 8,
+                              offset: Offset(0, 2),
                             )
-                          ],
-                        ),
+                          ]),
+                      clipBehavior: Clip.hardEdge,
+                      child: Row(
+                        children: [
+                          Image(image: AssetImage(movie.imageName)),
+                          SizedBox(width: 15),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(height: 15),
+                                Text(
+                                  movie.title,
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(height: 5),
+                                Text(movie.time,
+                                    maxLines: 1,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(color: Colors.grey)),
+                                SizedBox(height: 15),
+                                Text(
+                                  movie.description,
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                )
+                              ],
+                            ),
+                          ),
+                          SizedBox(width: 10),
+                        ],
                       ),
-                      SizedBox(width: 10),
-                    ],
-                  ),
+                    ),
+                    Material(
+                      color: Colors.transparent,
+                      child: InkWell(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        onTap: () {
+                          print('11');
+                        },
+                      ),
+                    )
+                  ],
                 ),
-                Material(
-                  color: Colors.transparent,
-                  child: InkWell(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                    onTap: () {
-                      print('11');
-                    },
-                  ),
-                )
-              ],
+              );
+            }),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: TextField(
+            controller: _searchController,
+            decoration: InputDecoration(
+              labelText: 'Поиск',
+              filled: true,
+              fillColor: Colors.white.withAlpha(400),
+              border: OutlineInputBorder(),
             ),
-          );
-        });
+          ),
+        )
+      ],
+    );
   }
 }
